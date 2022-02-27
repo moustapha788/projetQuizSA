d4f4cb32fadb6e51d10919e179b13d093db5299b
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-02-27 10:39:52 +0000

    Recherche d'un utilisateur dans le fichier json(notre base de données)

--- src/models/user.models.php
+++ src/models/user.models.php
@@ -2,3 +2,15 @@
 /***
  * !Toutes les requêtes d'informations par rapport à l'utilisateur
 */
+
+/* 
+! Recherche d'un utilisateur dans le fichier json(notre base de données) 
+*/
+function find_user_login_password(string $login,string $password):array{
+    $users=json_to_array("users");
+    foreach ($users as $user) {
+    if( $user['login']==$login && $user['password']==$password)
+        return $user;
+    }
+    return [];
+}
