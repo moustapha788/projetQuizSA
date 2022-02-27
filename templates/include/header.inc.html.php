460ef7299394f96b35b8ece5ba39367a978ebabb
Moustapha788 <gayemoustaphsmg97@gmail.com>
2022-02-27 21:46:38 +0000

    configuartion des includes

--- templates/include/header.inc.html.php
+++ templates/include/header.inc.html.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz App SA</title>
+    <link rel="stylesheet" href="<?= WEB_PUBLIC."css".DIRECTORY_SEPARATOR."style.common.css" ?>">
     <link rel="stylesheet" href="<?= WEB_PUBLIC."css".DIRECTORY_SEPARATOR."style.accueil.css" ?>">
     <link rel="stylesheet" href="<?= WEB_PUBLIC."css".DIRECTORY_SEPARATOR."style.connexion.css" ?>">
     <link rel="stylesheet" href="<?= WEB_PUBLIC."css".DIRECTORY_SEPARATOR."style.liste.joueur.css" ?>">
--- templates/include/menu.inc.html.php
+++ templates/include/menu.inc.html.php
@@ -1,9 +1,22 @@
+<header class="main-header">
+    <nav class="nav">
+        <ul class="navbar">
+            <li id="home"><a href="<?= /* Accueil */WEB_ROOT."?controller=user&action=accueil"?>">Accueil</a></li>
 
-<nav>
-    <ul>
-        <li><a href=""></a></li>
-    </ul>
-</nav>
+            <?php /* liste des joueurs */ if(is_admin()):?>
+                <li><a href="<?=WEB_ROOT?>">liste des joueurs</a></li>
+            <?php endif; ?>
+            
+            <?php /* inscription */ if(!is_connect()):?>
+                <li id="sign-in"><a href="<?=WEB_ROOT ?>">S'inscrire</a></li>
+            <?php endif; ?>
+
+            <?php /* deconnexion */ if(is_connect()):?>
+                <li id="log-out"><a href="<?=WEB_ROOT."?controller=securite&action=deconnexion"?>">Déconnexion</a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+</header>
 
 
 
